10e040e0d0d11c570442147d889af5d46aa7b8dc
agent14k <huntdavid175@gmail.com>
2020-08-18 13:43:55 +0000

    created functionality for choosing opponent

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Hello")
+let UIController = (function () {
+    const domLabels = {
+        cardsLabel: 'card',
+        firstWrapperLabel: 'first-wrapper',
+        secondWrapperLabel: 'second-wrapper',
+        gameWrapperLabel: 'game-wrapper'
+    }
+
+    const domElements = {
+        cards: document.querySelectorAll(`.${domLabels.cardsLabel}`),
+        firstWrapper: document.querySelector(`.${domLabels.firstWrapperLabel}`),
+        secondWrapper: document.querySelector(`.${domLabels.secondWrapperLabel}`),
+        gameWrapper: document.querySelector(`.${domLabels.gameWrapperLabel}`),
+    }
+
+    
+    domElements.cards.forEach(function(card) {
+        card.addEventListener('click', function() {
+            if (card.id == 'AI') {
+                domElements.firstWrapper.classList.add('no-display')
+                domElements.gameWrapper.classList.remove('no-display')
+                domElements.gameWrapper.classList.add('show')
+            }
+            else if (card.id == "friend") {
+                domElements.firstWrapper.classList.add('no-display');
+                domElements.secondWrapper.classList.remove('no-display');
+                domElements.secondWrapper.classList.add('show')
+            }
+        })
+    });
+})();
+
+
+let gameController = (function() {
+    // some code here 
+})();
