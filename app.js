8b8290c5f1b114b7b1ef1d7a6c8d1a99edf31484
agent14k <huntdavid175@gmail.com>
2020-08-29 11:59:25 +0000

    completed with AI mode

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// Logic controller 
+// Logic controller
 let logicController = (function () {
-   
   const Player = function (playerName, playerID) {
     let score = 0;
 
@@ -19,89 +18,97 @@ let logicController = (function () {
 
   let gameArray = ["", "", "", "", "", "", "", "", ""];
 
-      // function to determine winner 
-      let getWinner = function() {
-        let zero = gameArray[0];
-        let one = gameArray[1];
-        let two = gameArray[2];
-        let three = gameArray[3];
-        let four = gameArray[4];
-        let five = gameArray[5];
-        let six = gameArray[6];
-        let seven = gameArray[7];
-        let eight = gameArray[8];
-    
-        let winningCombos = [[zero,one,two],[three,four,five],[six,seven,eight],[zero,three,six],[one,four,seven],[two,five,eight],[zero,four,eight],[two,four,six]];
-        let deciderArray = [];
-        winningCombos.forEach(function(combo){
-            // Check if an array has the same symbols 
-           let decider = combo.every(function(item){
-               if (combo[0] != ''){
-                 return item == combo[0]
-               }
-           });
-           deciderArray.push(decider)
-        })
-    
-        // Check to see if there is a winner in the array ie. true 
-        let winnerDecider = deciderArray.some(function(item) {
-            return item == true
-        })
-    
-        // Get index of the occurence of true 
-        let indexOfWinner = deciderArray.indexOf(true);
-        let winningSymbol;
-    
-        // Get Symbol that won 
-        if(winnerDecider){
-            winningSymbol = winningCombos[indexOfWinner][0];
+  // function to determine winner
+  let getWinner = function () {
+    let zero = gameArray[0];
+    let one = gameArray[1];
+    let two = gameArray[2];
+    let three = gameArray[3];
+    let four = gameArray[4];
+    let five = gameArray[5];
+    let six = gameArray[6];
+    let seven = gameArray[7];
+    let eight = gameArray[8];
+
+    let winningCombos = [
+      [zero, one, two],
+      [three, four, five],
+      [six, seven, eight],
+      [zero, three, six],
+      [one, four, seven],
+      [two, five, eight],
+      [zero, four, eight],
+      [two, four, six],
+    ];
+    let deciderArray = [];
+    winningCombos.forEach(function (combo) {
+      // Check if an array has the same symbols
+      let decider = combo.every(function (item) {
+        if (combo[0] != "") {
+          return item == combo[0];
         }
-        
-        return winningSymbol
+      });
+      deciderArray.push(decider);
+    });
+
+    // Check to see if there is a winner in the array ie. true
+    let winnerDecider = deciderArray.some(function (item) {
+      return item == true;
+    });
+
+    // Get index of the occurence of true
+    let indexOfWinner = deciderArray.indexOf(true);
+    let winningSymbol;
+
+    // Get Symbol that won
+    if (winnerDecider) {
+      winningSymbol = winningCombos[indexOfWinner][0];
     }
 
-// function to generate random number for computer 
-let random = function() {
-  let randomNumber = Math.floor(Math.random() * 9)
-  return randomNumber;
-}
+    return winningSymbol;
+  };
 
-// function to add symbol for computer 
-let insert = function(addSym, checkWinner) {
-  let number = random();
-  let hasEnded = gameEnded()
+  // function to generate random number for computer
+  let random = function () {
+    let randomNumber = Math.floor(Math.random() * 9);
+    return randomNumber;
+  };
 
-  if(gameArray[number] == '' && !hasEnded) {
-    console.log("not ended")
-    addSym(number);
-  }
+  // function to add symbol for computer
+  let insert = function (addSym, checkWinner) {
+    let number = random();
+    let hasEnded = gameEnded();
 
-  else if( (gameArray[number] == 'x' || gameArray[number] == 'o') && !hasEnded){
-    console.log("not ended")
-    insert(addSym)
-  }
-  else if(hasEnded) {
-    console.log("gameEnded")
-    return
-  }
-}
+    if (gameArray[number] == "" && !hasEnded) {
+      console.log("not ended");
+      addSym(number);
+    } else if (
+      (gameArray[number] == "x" || gameArray[number] == "o") &&
+      !hasEnded
+    ) {
+      console.log("not ended");
+      insert(addSym);
+    } else if (hasEnded) {
+      console.log("gameEnded");
+      return;
+    }
+  };
 
-  // Function to check if game has ended or is still in progress 
-let gameEnded = function() {
-  let status = gameArray.every(function(item) {
-    return item != ''
-  })
-  return status
-}
+  // Function to check if game has ended or is still in progress
+  let gameEnded = function () {
+    let status = gameArray.every(function (item) {
+      return item != "";
+    });
+    return status;
+  };
 
-
-  // Reset gameArray function 
+  // Reset gameArray function
 
   function resetGameArray() {
-    gameArray.forEach(function(item, index) {
-      item = '';
+    gameArray.forEach(function (item, index) {
+      item = "";
       gameArray[index] = item;
-    })
+    });
   }
 
   return {
@@ -114,9 +121,7 @@ let gameEnded = function() {
   };
 })();
 
-
-
-// UI controller 
+// UI controller
 let UIController = (function () {
   // Names of dom element attributes
   const domLabels = {
@@ -132,7 +137,7 @@ let UIController = (function () {
     playerScoreLabel: "player-score",
     winnerDivLabel: "winner-div",
     winnerMessageLabel: "winner-message-label",
-    playAgainBtnLabel: "play-again"
+    playAgainBtnLabel: "play-again",
   };
 
   // Getting html elements
@@ -140,7 +145,9 @@ let UIController = (function () {
     cards: document.querySelectorAll(`.${domLabels.cardsLabel}`),
     firstWrapper: document.querySelector(`.${domLabels.firstWrapperLabel}`),
     secondWrapper: document.querySelector(`.${domLabels.secondWrapperLabel}`),
-    secondWrapperAI: document.querySelector(`.${domLabels.secondWrapperAILabel}`),
+    secondWrapperAI: document.querySelector(
+      `.${domLabels.secondWrapperAILabel}`
+    ),
     gameWrapper: document.querySelector(`.${domLabels.gameWrapperLabel}`),
     inputs: document.querySelectorAll(`.${domLabels.inputsLabel}`),
     playBtn: document.querySelector(`#${domLabels.playBtnLabel}`),
@@ -149,15 +156,15 @@ let UIController = (function () {
     playerScore: document.querySelectorAll(`.${domLabels.playerScoreLabel}`),
     winnerDiv: document.querySelector(`#${domLabels.winnerDivLabel}`),
     winnerMessage: document.querySelector(`#${domLabels.winnerMessageLabel}`),
-    playAgainBtn: document.querySelector(`#${domLabels.playAgainBtnLabel}`)
+    playAgainBtn: document.querySelector(`#${domLabels.playAgainBtnLabel}`),
   };
 
-  // function to remove classes on playing again 
+  // function to remove classes on playing again
 
   function removeClasses() {
-    domElements.boxes.forEach(function(box) {
-      box.classList.remove('blue-color', 'brown-color');
-    })
+    domElements.boxes.forEach(function (box) {
+      box.classList.remove("blue-color", "brown-color");
+    });
   }
 
   return {
@@ -167,7 +174,7 @@ let UIController = (function () {
   };
 })();
 
-// Game controller 
+// Game controller
 let gameController = (function (UICtrl, logicCtrl) {
   let roundState = 0;
   let gameMode;
@@ -183,24 +190,23 @@ let gameController = (function (UICtrl, logicCtrl) {
         UICtrl.UIDomElements.firstWrapper.classList.remove("show");
         UICtrl.UIDomElements.gameWrapper.classList.remove("no-display");
         UICtrl.UIDomElements.gameWrapper.classList.add("show");
-        gameMode = 'AI'
-        AI()
+        gameMode = "AI";
+        AI();
       } else if (card.id == "friend") {
         UICtrl.UIDomElements.firstWrapper.classList.add("no-display");
         UICtrl.UIDomElements.firstWrapper.classList.remove("show");
         UICtrl.UIDomElements.secondWrapper.classList.remove("no-display");
         UICtrl.UIDomElements.secondWrapper.classList.add("show");
-        gameMode = 'friendly'
+        gameMode = "friendly";
       }
     });
   });
 
-
   function AI() {
-    let player = logicCtrl.Player("Player", 'x')
-    let computer = logicCtrl.Player("Computer", "o")
+    let player = logicCtrl.Player("Player", "x");
+    let computer = logicCtrl.Player("Computer", "o");
 
-    playersArray.push(player,computer);
+    playersArray.push(player, computer);
     setPlayerInfo(playersArray);
   }
 
@@ -213,7 +219,7 @@ let gameController = (function (UICtrl, logicCtrl) {
         UICtrl.UIDomElements.boxes
       );
       playersArray.push(player);
-      input.value = ''
+      input.value = "";
     });
     UICtrl.UIDomElements.secondWrapper.classList.remove("show");
     UICtrl.UIDomElements.secondWrapper.classList.add("no-display");
@@ -225,12 +231,11 @@ let gameController = (function (UICtrl, logicCtrl) {
   // Function to add symbols to boxes
   let display = function () {
     boxes.forEach(function (box, index) {
-        if(logicCtrl.gameArray[index] == 'x') {
-            box.classList.add('brown-color')
-        }
-        else if (logicCtrl.gameArray[index] == 'o') {
-            box.classList.add('blue-color')
-        }
+      if (logicCtrl.gameArray[index] == "x") {
+        box.classList.add("brown-color");
+      } else if (logicCtrl.gameArray[index] == "o") {
+        box.classList.add("blue-color");
+      }
       box.innerHTML = ` ${logicCtrl.gameArray[index]} `;
     });
   };
@@ -244,30 +249,26 @@ let gameController = (function (UICtrl, logicCtrl) {
         let id = e.target.dataset.key;
         playersArray[roundState].addSymbol(id);
         display();
-        if(gameMode = 'friendly'){
-          declareWinner()
+        if ((gameMode = "friendly")) {
+          declareWinner();
         }
 
         if (roundState == 0) {
           roundState = 1;
 
-          // Computer's move 
-          if(gameMode == 'AI'){
-            logicCtrl.insert(playersArray[1].addSymbol)
-          display();
-          declareWinner()
-          roundState = 0
+          // Computer's move
+          if (gameMode == "AI") {
+            logicCtrl.insert(playersArray[1].addSymbol);
+            display();
+            declareWinner();
+            roundState = 0;
           }
-          
-          
         } else if (roundState == 1) {
           roundState = 0;
         }
       }
     });
   });
-
-
 
   // Function to set Usernames to UI
   let setPlayerInfo = function (players) {
@@ -282,73 +283,70 @@ let gameController = (function (UICtrl, logicCtrl) {
     UICtrl.UIDomElements.playerScore[1].textContent = playerTwoScore;
   };
 
-
-  // Function to declare winner 
-  let declareWinner = function() {
+  // Function to declare winner
+  let declareWinner = function () {
     let gameEnded = logicCtrl.gameEnded();
     let gameWinner = logicCtrl.getWinner();
     let winningMessage;
 
-    if (gameEnded && gameWinner == undefined ) {
-      winningMessage = "It's a draw"
-      UICtrl.UIDomElements.winnerDiv.classList.add('show-block');
-      UICtrl.UIDomElements.winnerMessage.textContent = winningMessage
+    if (gameEnded && gameWinner == undefined) {
+      winningMessage = "It's a draw";
+      UICtrl.UIDomElements.winnerDiv.classList.add("show-block");
+      UICtrl.UIDomElements.winnerMessage.textContent = winningMessage;
     }
- 
-        if (gameWinner == 'x'){
-          winningMessage = `${playersArray[0].playerName} is the Winner`
-          UICtrl.UIDomElements.winnerDiv.classList.add('show-block');
-          UICtrl.UIDomElements.winnerMessage.textContent = winningMessage
-            updateScore(playersArray[0])
-            winnerDeclared = true;
-        }
-        else if (gameWinner == 'o') {
-          winningMessage = `${playersArray[1].playerName} is the Winner`
-          UICtrl.UIDomElements.winnerDiv.classList.add('show-block');
-          UICtrl.UIDomElements.winnerMessage.textContent = winningMessage
-            updateScore(playersArray[1])
-            winnerDeclared = true;
-        }
-      }
 
+    if (gameWinner == "x") {
+      winningMessage = `${playersArray[0].playerName} is the Winner`;
+      UICtrl.UIDomElements.winnerDiv.classList.add("show-block");
+      UICtrl.UIDomElements.winnerMessage.textContent = winningMessage;
+      updateScore(playersArray[0]);
+      winnerDeclared = true;
+    } else if (gameWinner == "o") {
+      winningMessage = `${playersArray[1].playerName} is the Winner`;
+      UICtrl.UIDomElements.winnerDiv.classList.add("show-block");
+      UICtrl.UIDomElements.winnerMessage.textContent = winningMessage;
+      updateScore(playersArray[1]);
+      winnerDeclared = true;
+    }
+  };
 
-//   function to update score 
-function updateScore(player) {
-player.score += 1;
-setPlayerInfo(playersArray)
-}
+  //   function to update score
+  function updateScore(player) {
+    player.score += 1;
+    setPlayerInfo(playersArray);
+  }
 
-// function to reset score 
-function resetScore() {
-  playersArray[0].score = 0;
-  playersArray[1].score = 0;
-  setPlayerInfo(playersArray)
-}
+  // function to reset score
+  function resetScore() {
+    playersArray[0].score = 0;
+    playersArray[1].score = 0;
+    setPlayerInfo(playersArray);
+  }
 
-function playAgain() {
-  roundState = 0
-  UICtrl.UIDomElements.winnerDiv.classList.remove('show-block');
-  logicCtrl.resetGameArray();
-  display();
-  UICtrl.removeClasses()
-}
+  function playAgain() {
+    roundState = 0;
+    UICtrl.UIDomElements.winnerDiv.classList.remove("show-block");
+    logicCtrl.resetGameArray();
+    display();
+    UICtrl.removeClasses();
+  }
 
-// function to start a new game 
-function startNewGame(){
+  // function to start a new game
+  function startNewGame() {
     playAgain();
     resetScore();
     UICtrl.UIDomElements.gameWrapper.classList.remove("show");
     UICtrl.UIDomElements.gameWrapper.classList.add("no-display");
-    UICtrl.UIDomElements.firstWrapper.classList.remove('no-display')
-    UICtrl.UIDomElements.firstWrapper.classList.add('show');
+    UICtrl.UIDomElements.firstWrapper.classList.remove("no-display");
+    UICtrl.UIDomElements.firstWrapper.classList.add("show");
     gameMode = undefined;
-    playersArray = []
-}
+    playersArray = [];
+  }
 
   return {
     playersArray,
     display,
     playAgain,
-    startNewGame
+    startNewGame,
   };
 })(UIController, logicController);
